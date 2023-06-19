aaa6dcddebba2eec586591998f64676675978999
alexsavkaev <123101083+alexsavkaev@users.noreply.github.com>
2023-06-19 21:30:09 +0300

    added 2 tasks of seminar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,35 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//         Проверить, являются ли введённые строки, вращением друг друга.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первую строку: ");
+        String line1 = in.nextLine();
+        System.out.println("Введите вторую строку: ");
+        StringBuilder builder = new StringBuilder(in.nextLine()).reverse();
+        String line2 = builder.toString();
+        if(line1.equals(line2)) System.out.println("Да");
+        else System.out.println("Нет");
+
+        // 3+53=56
+        // Средствами StringBuilder поменять "=" на "равно"
+        StringBuilder builder1 = new StringBuilder("3+53=56");
+        builder1.replace(builder1.indexOf("="),
+                         builder1.indexOf("=")+1,
+                         " равно ");
+        System.out.println(builder1);
+        String  line = "Научиться составлять программы";
+        System.out.println(line.replace('а', 'б'));
+        StringBuilder line3 = new StringBuilder(line);
+//        for (int i = 0; i < line3.length(); i++) {
+//            line3.replace(line3.indexOf("а"),
+//                    line3.indexOf("а")+1,
+//                    "б");
+//        }
+        System.out.println(line3);
+
     }
 }
